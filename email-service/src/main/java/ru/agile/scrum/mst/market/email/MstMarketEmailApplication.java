1dbeb912c94db30b9df84d00c2bddc6bd70a8a75
dvpesh <dvpesh@mail.ru>
2023-03-30 18:09:29 +0300

    исправлен баг в задаче 8.1

--- email-service/src/main/java/ru/agile/scrum/mst/market/email/MstMarketEmailApplication.java
+++ email-service/src/main/java/ru/agile/scrum/mst/market/email/MstMarketEmailApplication.java
@@ -1,11 +1,9 @@
 package ru.agile.scrum.mst.market.email;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class MstMarketEmailApplication {
 
     public static void main(String[] args) {
