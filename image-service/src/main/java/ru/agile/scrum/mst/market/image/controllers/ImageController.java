8e23b905ae17265a61b07334297243ae083e618d
VorobevOleg <kuruzuza-09@ya.ru>
2023-04-09 15:31:04 +0300

    Удалены лишние импорты

--- image-service/src/main/java/ru/agile/scrum/mst/market/image/controllers/ImageController.java
+++ image-service/src/main/java/ru/agile/scrum/mst/market/image/controllers/ImageController.java
@@ -1,20 +1,14 @@
 package ru.agile.scrum.mst.market.image.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.agile.scrum.mst.market.api.ImageDto;
 import ru.agile.scrum.mst.market.image.converters.ImageConverter;
-import ru.agile.scrum.mst.market.image.models.Image;
 import ru.agile.scrum.mst.market.image.services.ImageService;
 
 import java.io.IOException;
-import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/v1/image")
