4520aa116a4685b3383aa358fbfbe1360c23cf4f
dvpesh <dvpesh@mail.ru>
2023-05-01 00:39:24 +0300

    выполнена задача [#32] добавлен профиль dev для запуска приложения из IntelliJ IDEA

--- cart-service/src/main/java/ru/agile/scrum/mst/market/cart/configs/RedisConfig.java
+++ cart-service/src/main/java/ru/agile/scrum/mst/market/cart/configs/RedisConfig.java
@@ -2,8 +2,7 @@ package ru.agile.scrum.mst.market.cart.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -12,18 +11,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory(new RedisStandaloneConfiguration("redis", 6379));
-//        return new JedisConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
-    }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
         return template;
     }
 }
